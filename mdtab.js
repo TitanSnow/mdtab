d38464bfbd8f623b3cf0f65daafcbc1710a26035
TitanSnow <sweeto@live.cn>
2017-03-01 19:12:41 +0800

    initial commit

---
+++ mdtab.js
@@ -0,0 +1,75 @@
+;(function(global,undefined){
+	"use strict"
+	marked.setOptions({
+		highlight: function (code) {
+		return hljs.highlightAuto(code).value;
+		}
+	})
+	global.mdtab=function(width,height){
+		width=Number(width)
+		height=Number(height)
+		if(isNaN(width)||isNaN(height))
+			return null
+		var nd=document.createElement("div")
+		nd.style.width=width+"px"
+		nd.style.height=height+"px"
+		nd.className="mdtab"
+		var btn_edit=document.createElement("button")
+		var btn_prev=document.createElement("button")
+		var btn_sour=document.createElement("button")
+		btn_edit.type="button"
+		btn_prev.type="button"
+		btn_sour.type="button"
+		btn_edit.className="mdtab-tabbtn mdtab-tabchecked"
+		btn_prev.className="mdtab-tabbtn"
+		btn_sour.className="mdtab-tabbtn"
+		btn_edit.textContent="Edit"
+		btn_prev.textContent="Preview"
+		btn_sour.textContent="HTML"
+		var tab=document.createElement("div")
+		tab.className="mdtab-tab"
+		tab.appendChild(btn_edit)
+		tab.appendChild(btn_prev)
+		tab.appendChild(btn_sour)
+		nd.appendChild(tab)
+		var edit=document.createElement("div")
+		var prev=document.createElement("div")
+		var sour=document.createElement("pre")
+		var valc=document.createElement("div")
+		edit.className="mdtab-val mdtab-active"
+		prev.className="mdtab-val"
+		sour.className="mdtab-val"
+		valc.className="mdtab-valueconter"
+		nd.appendChild(valc)
+		valc.appendChild(edit)
+		valc.appendChild(prev)
+		valc.appendChild(sour)
+		var cm=CodeMirror(edit,{
+			"mode":"markdown"
+		})
+		cm.setSize(width,height-30)
+		function swch(){
+			nd.querySelector(".mdtab-tabchecked").classList.remove("mdtab-tabchecked")
+			nd.querySelector(".mdtab-active").classList.remove("mdtab-active")
+			this.classList.add("mdtab-tabchecked")
+		}
+		btn_edit.addEventListener("click",swch)
+		btn_prev.addEventListener("click",swch)
+		btn_sour.addEventListener("click",swch)
+		btn_edit.addEventListener("click",function(){
+			edit.classList.add("mdtab-active")
+		})
+		btn_prev.addEventListener("click",function(){
+			prev.classList.add("mdtab-active")
+			prev.innerHTML=marked(cm.getValue())
+		})
+		btn_sour.addEventListener("click",function(){
+			sour.classList.add("mdtab-active")
+			sour.textContent=marked(cm.getValue())
+		})
+		return {
+			"element":nd,
+			"textarea":cm
+		}
+	}
+})(this);
